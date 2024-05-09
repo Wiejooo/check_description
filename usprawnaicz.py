6ef0d4d338f0d46f4ca8a42e37a2bb0634f56825
jwiejak <jakub.wiejak@softax.pl>
2024-05-09 15:55:20 +0200

    usprawnienie

--- usprawnaicz.py
+++ usprawnaicz.py
@@ -1,63 +1,6 @@
 import pandas as pd
 import importlib
 
-def test_02_PraUPP_Wyszukiwarka():
-
-
-    """
-    |
-    | **Przypadek testowy: 2**
-
-    | **Tytuł:** Pracownicy - Uprawnienia Pracowników - Przywileje - Wyszukiwarka
-
-    | **Warunki początkowe:**
-
-    | 1. W systemie są przywileje.
-    | 2. Operator systemu posiada uprawnienia: SA-EMPLOYEE-PRIVILEGES-LIST:ACCESS.
-
-    | **Kroki:**
-
-    | 1. Operator loguje się do serwisu SA
-    | 2. Operator wchodzi w "Pracownicy"
-    | 3. Operator rozwija listę "Uprawnienia Pracowników"
-    | 4. Operator wchodzi w "Przywileje"
-    | 5. Operator uzupełnia "Czynność"
-    | 6. Operator uzupełnia "Modyfikator"
-    | 7. Operator klika "Szukaj"
-
-    | **Oczekiwany rezultat:** System wyświetla ekran "Przywileje" z wyszukiwarką przywilejów. System
-                                wyświetla listę przywilejów spełniających kryteria wyszukiwania.
-
-    | **Warunki końcowe:** Ekran "Przywileje" z szukanymi rekordami.
-
-    """
-
-def test_03_PraUPP_WyszukiwarkaWyczysc():
-    """
-    |
-    | **Przypadek testowy: 3**
-
-    | **Tytuł:** Pracownicy - Uprawnienia Pracownikow - Przywileje - Wyszukiwarka - Wyczyść
-
-    | **Warunki początkowe:** Operator systemu posiada uprawnienia: SA-EMPLOYEE-PRIVILEGES-LIST:ACCESS.
-
-    | **Kroki:**
-
-    | 1. Operator loguje się do serwisu SA
-    | 2. Operator wchodzi w "Pracownicy"
-    | 3. Operator rozwija listę "Uprawnienia Pracowników"
-    | 4. Operator wchodzi w "Przywileje"
-    | 5. Operator uzupełnia "Czynność"
-    | 6. Operator uzupełnia "Modyfikator"
-    | 7. Operator klika "Wyczyść"
-
-    | **Oczekiwany rezultat:** System wyświetla ekran "Przywileje" z wyszukiwarką przywilejów.
-                                System czyści pola wyszukiwania wypełnione przez Operatora systemu.
-
-    | **Warunki końcowe:** Kryteria wyszukiwania zostają wyczyszczone.
-
-    """
-
 # Wczytanie pliku CSV
 df = pd.read_csv('check_description/SA_Pracownicy.csv')
 
